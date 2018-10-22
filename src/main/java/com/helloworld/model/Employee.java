95c25652e2730c6fea5f723c5b5d3bc9df4ceb27
Reem Flaifel <reem.flaifl@gmail.com>
2018-10-22 22:52:03 +0300

    Implementing employee model

---
+++ src/main/java/com/helloworld/model/Employee.java
@@ -0,0 +1,28 @@
+package com.helloworld.model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Employee {
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
